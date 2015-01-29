31dbe2a055c986b068f00af255608488e50636a2
WoWoX <tommylocklear0@gmail.com>
2015-01-29 13:08:50 -0500

    java

---
+++ java/mylife/src/my/life/MathHandler.java
@@ -0,0 +1,22 @@
+package my.life;
+
+import com.smartfoxserver.v2.entities.User;
+import com.smartfoxserver.v2.entities.data.ISFSObject;
+import com.smartfoxserver.v2.entities.data.SFSObject;
+import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
+
+public class MathHandler extends BaseClientRequestHandler {
+
+	public void handleClientRequest(User player, ISFSObject params) 
+	{
+		int n1 = params.getInt("n1");
+		int n2 = params.getInt("n2");
+		
+		ISFSObject rtn = new SFSObject();
+		rtn.putInt("sum", n1 + n2);
+		
+		MylifeExtension parentEx = (MylifeExtension) getParentExtension();
+		parentEx.send("math", rtn, player);
+	}
+
+}
